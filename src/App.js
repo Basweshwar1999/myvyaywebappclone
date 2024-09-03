f84be48fbd13e0d6ef7102420c6b1834d5e66e0c
basweshwarbotmatic <basweshwar.g@botmaticsolution.in>
2024-09-03 19:09:58 +0530

    api configuration

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'; // Updated imports
 import LoginPage from './Component/Code/Login';
@@ -20,3 +21,24 @@ function App() {
 }
 
 export default App;
+=======
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'; // Updated imports
+import LoginPage from './Component/Code/Login';
+import Dashboard from './Component/Code/Dashboard';
+import CreateTravelReq from './Component/Code/CreateTravelReq';
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/CreateTravelReq" element={<CreateTravelReq />} />
+        <Route path="/" element={<Navigate to="/login" />} /> {/* Replaces Redirect */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+>>>>>>> d040df8 (api configuration)
