1408a1b60b7b18115abecac4d3fcfd027d5d8769
Basweshwar Gubge <basweshwar.g@botmaticsolution.in>
2025-01-28 12:43:34 +0530

    Conflict resolved

--- src/App.js
+++ src/App.js
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'; // Updated imports
-import LoginPage from './Component/Code/Login';
-import Dashboard from './Component/Code/Dashboard';
-import CreateTravelReq from './Component/Code/CreateTravelReq';
-import CreGenReq from './Component/Code/CreGenReq';
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/CreateTravelReq" element={<CreateTravelReq />} />
-        <Route path="/CreGenReq" element={<CreGenReq />} />
-        <Route path="/" element={<Navigate to="/login" />} /> {/* Replaces Redirect */}
-      </Routes>
-    </Router>
-  );
-}
-
-export default App;
-=======
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'; // Updated imports
 import LoginPage from './Component/Code/Login';
@@ -41,4 +17,3 @@ function App() {
 }
 
 export default App;
->>>>>>> d040df8 (api configuration)
